93a8c41ed79f98ee75660a31e98133e5fe2ac640
RogérioReiff <rogerioreiff@outlook.com>
2024-03-28 18:12:44 -0300

    Adicionando Arquivos

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View} from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.Header}>
+            <Text style={styles.Title}>My Todo's</Text>
+        </View>
+    )
+}
+
+const styles =  StyleSheet.create({
+    Header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: 'coral',
+    },
+    Title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+});
