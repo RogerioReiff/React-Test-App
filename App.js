d2ee554fce20b1c7d0192764625c00287a42a9c2
RogérioReiff <rogerioreiff@outlook.com>
2024-04-01 13:40:40 -0300

    Atualização do expo + pacotes

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useState } from 'react';
 import Home from './screens/home';
 import { useFonts } from 'expo-font';
-import { AppLoading } from 'expo'; 
+import AppLoading from 'expo-app-loading';
+
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
   }); 
+
 
    if(!fontsLoaded){
     <AppLoading />
