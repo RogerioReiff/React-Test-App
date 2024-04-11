18f70ee0eee8f5d0d6de253295887ed488f4d2c7
RogérioReiff <rogerioreiff@outlook.com>
2024-04-11 10:04:01 -0300

    Passando dados entre telas diferentes + usando FlatList and route

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetail(){
+export default function ReviewDetail({route, navigation}){
+
+    const {Items} = route.params;
+
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Detail Screen</Text>
+            <Text style={globalStyles.titleText}>{Items.title}</Text>
+            <Text style={globalStyles.titleText}>{Items.body}</Text>
+            <Text style={globalStyles.titleText}>{Items.rating}</Text>
         </View>
     );
 }
