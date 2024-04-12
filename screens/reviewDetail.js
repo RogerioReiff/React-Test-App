cd89f831eb8aad03860d7bb7ec92757f459dd4ba
RogérioReiff <rogerioreiff@outlook.com>
2024-04-11 22:12:02 -0300

    Adicionando imagens e as associando as notas

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetail({route, navigation}){
@@ -12,8 +12,22 @@ export default function ReviewDetail({route, navigation}){
             <Card>
                 <Text style={globalStyles.titleText}>{Items.title}</Text>
                 <Text style={globalStyles.titleText}>{Items.body}</Text>
-                <Text style={globalStyles.titleText}>{Items.rating}</Text>
+                <View style={styles.rating}>
+                    <Text>BroZone rating: </Text>
+                    <Image source={images.ratings[Items.rating]}/>
+                </View>
             </Card>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+});
